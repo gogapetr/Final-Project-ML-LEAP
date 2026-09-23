bb7cffc83e44790bada0f01f5c8d085dd2ace901
gogapetr <petrid.georgia@gmail.com>
2026-09-23 12:07:32 +0300

    commit 1a2b3c4d5e6f7g8h9i0j

--- Notebook.py
+++ Notebook.py
@@ -40,7 +40,7 @@ print("Positive rate (share earning >50K):", round(y.mean(), 3))
 
 ##  train and test split
 
-X_train, X_test, y_train, y_test = train_te st_split(
+X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42, stratify=y)
 
 print("Training rows:", X_train.shape[0])
